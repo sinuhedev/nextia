d4f0f6a0a335b13f2433be70102ebd5fc658190c
Sinuhe Maceda <sinuhe.dev@gmail.com>
2026-02-12 23:42:45 -0600

    fix(hooks) Fixed useQueryString hook

--- template/src/utils/hooks.js
+++ template/src/utils/hooks.js
@@ -1,23 +1,26 @@
 import { useEffect, useState } from 'react'
 
 export function useQueryString() {
-  const getQueryString = () => ({
-    hash: window.location.hash.split('?')[0],
-    queryString: Object.fromEntries(
-      new URLSearchParams(window.location.hash.split('?')[1])
-    )
-  })
+  const getQueryString = useCallback(
+    () => ({
+      hash: window.location.hash.split('?')[0],
+      queryString: Object.fromEntries(
+        new URLSearchParams(window.location.hash.split('?')[1])
+      )
+    }),
+    []
+  )
 
-  const [queryString, setQueryString] = useState(getQueryString())
+  const [queryString, setQueryString] = useState(getQueryString)
 
   useEffect(() => {
-    window.addEventListener('popstate', () => setQueryString(getQueryString()))
+    const handlePopState = () => setQueryString(getQueryString())
+
+    window.addEventListener('popstate', handlePopState)
     return () => {
-      window.removeEventListener('popstate', () =>
-        setQueryString(getQueryString())
-      )
+      window.removeEventListener('popstate', handlePopState)
     }
-  }, [])
+  }, [getQueryString])
 
   return queryString
 }
