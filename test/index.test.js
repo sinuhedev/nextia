1ab911d260fb56106f529e6e341bed72e1814b9c
Sinuhe Maceda <sinuhe.dev@gmail.com>
2026-03-17 15:12:15 -0600

    updated

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,4 @@
-import { css, i18n } from 'lib'
+import { css } from 'lib'
 import { test } from 'vitest'
 
 test('cssTest', () => {
@@ -16,17 +16,5 @@ test('cssTest', () => {
       '    my-class css-red   ',
       'bg-red'
     )
-  )
-})
-
-test('i18nTest', () => {
-  const i18nFile = {
-    locales: ['EN', 'ES'],
-    defaultLocale: 'ES',
-    name: ['My name is {0} {1} {2}', 'Mi nombre es {0} {1} {2}']
-  }
-
-  console.log(
-    i18n('name', ['Sinuhe', 'Maceda', 'Bouchan'], i18nFile, i18nFile.locales[0])
   )
 })
