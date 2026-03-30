b3d8724c91a6fb14275ee195acd9b5c5fd229a4b
Sinuhe Maceda <sinuhe.dev@gmail.com>
2026-03-30 04:44:21 -0600

    updated licenses

--- src/lib/ui.js
+++ src/lib/ui.js
@@ -1,3 +1,12 @@
+/**
+ * Copyright (c) 2025 Sinuhe Maceda https://sinuhe.dev
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * https://github.com/sinuhedev/nextia
+ */
+
 import { createElement, useEffect, useRef } from 'react'
 import { useFx } from './fx'
 import { css } from './utils'
