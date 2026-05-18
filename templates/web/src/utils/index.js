6d1db89d17273d659800f6a2c49fbe360784205b
Sinuhe Maceda <sinuhe.dev@gmail.com>
2026-05-18 01:28:55 -0600

    updated

--- templates/web/src/utils/index.js
+++ templates/web/src/utils/index.js
@@ -1,14 +1,10 @@
 import { version } from 'nextia'
 
-const env = import.meta.env
-
-env.WINDOW_RESIZE = {
-  md: 640,
-  lg: 1024,
-  xl: 1280
-}
-
-env.VERSION = version()
+const env = Object.freeze({
+  ...import.meta.env,
+  VERSION: version(),
+  WINDOW_RESIZE: { md: 640, lg: 1024, xl: 1280 }
+})
 
 function sum(a, b) {
   return a + b
