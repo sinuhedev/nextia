859ac93c66e9f209a3df2abb919f2fd63aa569d6
Sinuhe Maceda <sinuhe.dev@gmail.com>
2026-09-21 01:00:50 -0600

    updated

--- examples/bunjs/src/utils/index.js
+++ examples/bunjs/src/utils/index.js
@@ -4,7 +4,7 @@ const env = Object.freeze({
   VERSION: getVersion(),
   HOME_PAGE: '#/home',
   VIEW_TRANSITION_NAME: 'fade',
-  API: process.env.PUBLIC_API
+  PUBLIC_API: process.env.PUBLIC_API
 })
 
 export { env }
